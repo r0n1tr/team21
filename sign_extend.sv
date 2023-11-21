710bf653d9513d02df68bf8c8a2a1ef1ab77db4b
za722-ic <za722@ic.ac.uk>
2023-11-21 08:11:49 +0000

    Implemented sign extension Input immediate value is taken from bits of instr (which bits is determined by ImmSrc). Concatenate appropriate bits from input to create 32-bit output word, and place word on ImmOp line

--- sign_extend.sv
+++ sign_extend.sv
@@ -4,4 +4,11 @@ module sign_extend(
     output logic [31:0] ImmOp   // Immediate operand - is the sign extended output of this module
 );
 
+always_comb begin
+    if (ImmSrc) // executing addi
+        ImmOp = { {20{instr[31]}} , instr[31:12] }; // sign extend 12-bit immediate value to give 32-bit output
+    else        // executing bne
+        ImmOp = { {22{0}}         , instr[30:25], instr[11:8] }; // bne has an address as its immediate, so zero-extend it instead of sign-extending
+end
+
 endmodule
