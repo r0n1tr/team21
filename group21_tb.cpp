f01d8145feecd47b363481778a04c5fc7a480039
MohammedTK22 <133985295+MohammedTK22@users.noreply.github.com>
2023-11-21 14:50:19 +0000

    Add files via upload

---
+++ group21_tb.cpp
@@ -0,0 +1,45 @@
+#include "vbuddy.cpp"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "VCPU.h" // change if sv file is named something else
+
+
+int main(int argc, char **argv, char **env){
+    int i;
+    int clk;
+
+    Verilated::commandArgs(argc, argv);
+    // init top verilog instance
+    Vcounter* top = new Vcounter; // change if sv file is named something else
+    // init trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace (tfp, 99);
+    tfp->open ("counter.vcd"); // change if sv file is named something else
+
+    // init Vbuddy
+    if (vbdOpen() != 1) return(-1);
+    vbdHeader("IAC CW");
+
+    // initialize simulation inputs
+    top->clk = 1;
+    top->rst = 1;
+    top->en = 0;
+
+    // run simulation for many clock cycles
+    for (i=0; i<1000000; i++){
+
+        // dump variables into VCD file and toggle clock
+        for(clk=0; clk<2; clk++){
+            tfp->dump  (2*i+clk);
+            top->clk = !top->clk;
+            top->eval ();
+        }
+         top->rst = 0;
+        if (Verilated::gotFinish())     exit(0);
+    }
+
+    vbdClose();     
+    tfp->close();
+    exit(0);
+}
