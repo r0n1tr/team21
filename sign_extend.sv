19d7574d9bc318590ffc8cfc02f5664db6a11035
za722-ic <za722@ic.ac.uk>
2023-11-21 08:02:39 +0000

    Added comments for input/output signals

--- sign_extend.sv
+++ sign_extend.sv
@@ -1,9 +1,7 @@
 module sign_extend(
-    input  logic [31:0] instr,
-    input  logic        ImmSrc,
-    output logic [31:0] ImmOp
+    input  logic [31:0] instr,  // 32-bit instruction word
+    input  logic        ImmSrc, // Flag to tell this module which isntruction type is being executed (and hence which bits form the immediate and whether they need to be sign extended)
+    output logic [31:0] ImmOp   // Immediate operand - is the sign extended output of this module
 );
-
-
 
 endmodule
