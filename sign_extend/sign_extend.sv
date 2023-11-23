8585dad5bca16f42b10c1faf73c6b41c46c19527
za722-ic <za722@ic.ac.uk>
2023-11-23 21:44:29 +0000

    Changed zero extension of immediate of instruction bne to regular sign extension zero extension was incorrect - the immediate value is an offset to current address and hence CAN be negative look at the machine code given in Lab 4 as an example - convert the hex to binary, pick out the bits that make up the immediate, and convert to decimal. The two's complement value makes actual sense unlike the unsigned value

--- sign_extend/sign_extend.sv
+++ sign_extend/sign_extend.sv
@@ -1,7 +1,7 @@
 // verilator lint_off UNUSED
 module sign_extend(
     input  logic [31:0] instr,  // 32-bit instruction word
-    input  logic        ImmSrc, // Flag to tell this module which isntruction type is being executed (and hence which bits form the immediate and whether they need to be sign extended)
+    input  logic        ImmSrc, // Flag to tell this module which isntruction type is being executed (and hence which bits in instr form the immediate)
     output logic [31:0] ImmOp   // Immediate operand - is the sign extended output of this module
 );
 
@@ -9,7 +9,7 @@ always_comb begin
     if (ImmSrc) // executing addi
         ImmOp = { {12{instr[31]}} , instr[31:12] }; // sign extend 12-bit immediate value to give 32-bit output
     else        // executing bne
-        ImmOp = { {22{1'b0}}      , instr[30:25], instr[11:8] }; // bne has an address as its immediate, so zero-extend it instead of sign-extending
+        ImmOp = { {22{instr[30]}} , instr[30:25] , instr[11:8] }; // sign extend 10-bit immediate value to give 32-bit output; bne's immediate value is used as an offset for the PC (so can be negative or positive, so needs to be sign extended) 
 end
 
 endmodule
