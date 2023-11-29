182d653ee53edd48ea36e234f0cc32c42a5c07bb
za722-ic <za722@ic.ac.uk>
2023-11-29 23:41:59 +0000

    Created testbench for control unit
    Testbench tests five instructions.
    
    Each instruction's machine code is provided to the control unit
    and the output is printed for when zero flag is both 0 and 1.
    
    The tested instructions are: addi, lw, sw, or, beq

--- control_unit/top_control_unit_tb.cpp
+++ control_unit/top_control_unit_tb.cpp
@@ -1,43 +1,49 @@
-#include "Vcontrol_unit.h"
+#include "Vtop_control_unit.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 #include <iostream>
 #include <iomanip>
 #include <vector> 
 
-void printTopState(const Vcontrol_unit* top)
+void printTopState(const Vtop_control_unit* top, std::string instr_name)
 {
+    std::cout << instr_name << "   ";
     std::cout << "0x" << std::setfill('0') << std::setw(8) << std::hex << top->instr << std::dec << "   ";
-    std::cout << "EQ = " << ((top->EQ) ? "1":"0") << ":   ";
+    std::cout << "ZERO = " << ((top->zero) ? "1":"0") << ":   ";
 
-    std::cout << ((top->RegWrite) ? "1":"0");
-    std::cout << ((top->ALUctrl ) ? "1":"0");
-    std::cout << ((top->ALUsrc  ) ? "1":"0");
-    std::cout << ((top->ImmSrc  ) ? "1":"0");
-    std::cout << ((top->PCsrc   ) ? "1":"0") << std::endl;
+    std::cout << ((top->RegWrite ) ? "1":"0")  << "        ";
+    std::cout << (std::bitset<2>(top->ImmSrc)) << "     ";
+    std::cout << ((top->ALUSrc   ) ? "1":"0")  << "      ";
+    std::cout << ((top->MemWrite ) ? "1":"0")  << "        ";
+    std::cout << ((top->ResultSrc) ? "1":"0")  << "         ";
+    std::cout << ((top->PCsrc    ) ? "1":"0")  << "     ";
+    std::cout << (std::bitset<3>(top->ALUControl)) << std::endl;;
 }
 
 int main(int argc, char **argv, char **env) {
     Verilated::commandArgs(argc,argv);
 
     // init top verilog instance
-    Vcontrol_unit* top = new Vcontrol_unit;
+    Vtop_control_unit* top = new Vtop_control_unit;
 
     // init trace dump
     Verilated::traceEverOn(true);
     VerilatedVcdC* tfp = new VerilatedVcdC;
     top->trace(tfp, 99);
-    tfp->open("control_unit.vcd");
+    tfp->open("top_control_unit.vcd");
 
     // init memory contents
-    std::vector<unsigned int> mem_contents = {0x0FF00313,0x00000513,0x00000593,0x00058513,0x00158593,0xFE659CE3,0xFE0318E3};
+    std::vector<std::string>  instr_names  = {"addi",     "lw  ",     "sw  ",     "or  ",     "beq "};
+    std::vector<unsigned int> mem_contents = {0x0FF00313, 0xFFC4A303, 0x0064A423, 0x0062E233, 0xFE420AE3};
+
+    std::cout << std::endl << "                                RegWrite ImmSrc ALUSrc MemWrite ResultSrc PCsrc ALUControl" << std::endl << std::endl;
 
     // run simulation for 2 clock cycles per instruction; once for EQ = 0 and agaiin for EQ = 1.
     for (int i = 0; i < mem_contents.size()*2; i++)
     {
         // set simulation inputs
         top->instr = mem_contents[i / 2];
-        top->EQ = (i % 2 != 0);
+        top->zero = (i % 2 != 0);
 
         // dump variables into VCD file and evaluate simulation for clock low then high
         for (int clk = 0; clk < 2; clk++)
@@ -47,7 +53,8 @@ int main(int argc, char **argv, char **env) {
         }
 
         // print current state of top
-        printTopState(top);
+        printTopState(top, instr_names[i / 2]);
+        if (i % 2 == 1) std::cout << std::endl;
 
         if (Verilated::gotFinish()) exit(0);
     }
