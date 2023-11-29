00e28f61d6ca3c7a482cb7710fd8d4cda8856d02
za722-ic <za722@ic.ac.uk>
2023-11-29 22:31:08 +0000

    Formatting

--- reg_file/reg_file.sv
+++ reg_file/reg_file.sv
@@ -2,9 +2,9 @@ module reg_file #(
     parameter REG_FILE_ADDR_WIDTH = 5, 
               DATA_WIDTH = 32
 )(
-    input  logic                          clk, 
-    input  logic                          WE3, // write enable 
-    input  logic [DATA_WIDTH-1:0]         WD3,
+    input logic                           clk, 
+    input logic                           WE3, // write enable 
+    input logic [DATA_WIDTH-1:0]          WD3,
     input logic [REG_FILE_ADDR_WIDTH-1:0] AD1,
     input logic [REG_FILE_ADDR_WIDTH-1:0] AD2,
     input logic [REG_FILE_ADDR_WIDTH-1:0] AD3, 
@@ -17,7 +17,7 @@ module reg_file #(
 logic [DATA_WIDTH-1:0] ram_array [2**REG_FILE_ADDR_WIDTH-1:0];
 
 always_ff @(posedge clk) begin
-    if(WE3 == 1'b1) ram_array[AD3] <= WD3;
+    if(WE3) ram_array[AD3] <= WD3;
 end
 
 assign RD1 = ram_array[AD1];
