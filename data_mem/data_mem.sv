8609fc69aad977c247c20eb26d4ba9dccfbdcd5b
za722-ic <za722@ic.ac.uk>
2023-11-29 22:26:46 +0000

    Move files from Data_mem_tb
    These files were in Data_mem_tb and have been moved into the main
    data_mem folder (with some added formatting)

--- data_mem/data_mem.sv
+++ data_mem/data_mem.sv
@@ -1,15 +1,13 @@
 module data_mem #(
-
-    parameter ADDRESS_WIDTH = 8, //it will come as 32 bits but we will only take the first 8
-              DATA_WIDTH = 32
-   
+    parameter ADDRESS_WIDTH = 8, // comes as 32 bits but we will only take the first 8
+              DATA_WIDTH = 8     // this used to be 32
 )(
+    input  logic                  clk, // only for writing to data_mem
+    input  logic                  WE,  // write enable flag
+    input  logic [DATA_WIDTH-1:0] WD,  // the data we receieve from rs2
+    input  logic [DATA_WIDTH-1:0] A,   // the address that comes from ALUResult irrespective we are loading or storing
 
-    input  logic clk,   //only for Writing to Data Memory 
-    input  logic   WE,   // Write Enable flag
-    input  logic signed [DATA_WIDTH-1:0] WD,  //The Data we recieve from the rs2
-    input  logic [DATA_WIDTH-1:0] A,   // The Adress that comes from ALU Result irrespective we are loading or stroring
-    output logic signed [DATA_WIDTH-1:0] RD // Data read from the memory
+    output logic [DATA_WIDTH-1:0] RD   // data read from the memory
 );   
 
 
@@ -17,14 +15,13 @@ logic [DATA_WIDTH-1:0] data_memory [2**ADDRESS_WIDTH-1:0];
 
 initial begin
     $display("Loading ram...");
-    $readmemh("ADD FILE ", ram_array); 
+    $readmemh("ram_array.mem", data_memory); 
 end;
 
-
 always_ff @(posedge clk) begin
-    if (WE == 1'b1) data_memory[A] <= WD; // we are storing word 
+    if (WE) data_memory[A] <= WD; // storing word (only on clk rising edge)
 end
 
-    if(WE == 1'b0) assign RD = data_memory[A]; // we are loading word 
+assign RD = data_memory[A]; // loading word (asynchronously)
 
 endmodule
