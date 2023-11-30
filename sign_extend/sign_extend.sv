2ab1d60767145e3e77e513c9dc6c7dcad3e61207
za722-ic <za722@ic.ac.uk>
2023-11-30 00:53:49 +0000

    Updated sign extend block
    Sign extension now generalised to work for
    I-, S-, B-type instructions.

--- sign_extend/sign_extend.sv
+++ sign_extend/sign_extend.sv
@@ -1,15 +1,18 @@
 // verilator lint_off UNUSED
 module sign_extend(
     input  logic [31:0] instr,  // 32-bit instruction word
-    input  logic        ImmSrc, // Flag to tell this module which isntruction type is being executed (and hence which bits in instr form the immediate)
+    input  logic [1:0]  ImmSrc, // Indicates instruction type, and hence which bits of the instruction contain the immediate
     output logic [31:0] ImmOp   // Immediate operand - is the sign extended output of this module
 );
 
+// Sign extension implementation (as defined in lecture 7, slide 15)
 always_comb begin
-    if (ImmSrc) // executing bne
-        ImmOp = { {20{instr[31]}} , instr[7], instr[30:25] , instr[11:8], {1'b0} }; // sign extend 10-bit immediate value to give 32-bit output; bne's immediate value is used as an offset for the PC (so can be negative or positive, so needs to be sign extended) 
-    else        // executing addi
-        ImmOp = { {20{instr[31]}} , instr[31:20] }; // sign extend 12-bit immediate value to give 32-bit output
+    case (ImmSrc)
+        2'b00:   ImmOp = { {20{instr[31]}} , instr[31:20] };                                      // I-TYPE; sign extend 12-bit imm
+        2'b01:   ImmOp = { {20{instr[31]}} , instr[31:25] , instr[11:7] };                        // S-TYPE; sign extend 12-bit imm
+        2'b10:   ImmOp = { {20{instr[31]}} , instr[7]     , instr[30:25] , instr[11:8], {1'b0} }; // B-TYPE; sign extend 13-bit imm; ImmOp used as offset to PC (so can be -ve or +ve, so needs to be sign extended)
+        default: ImmOp = { 32{1'b0} }; // e.g. for R-TYPE (as it has no immediate)
+    endcase
 end
 
 endmodule
