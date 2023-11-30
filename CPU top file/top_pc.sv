4544030b6a6d954c96cc0f17a245b83207f7ef65
daniald1e <danial.dehghan22@imperial.ac.uk>
2023-11-30 11:29:38 +0000

    added two new multiplexers into pc top so trigger would work

--- CPU top file/top_pc.sv
+++ CPU top file/top_pc.sv
@@ -6,12 +6,13 @@ module top_pc #(
     input  logic                     rst,
     input  logic                     PCsrc,
     input  logic [DATA_WIDTH-1:0]    ImmExt,
-
-    output logic [ADDRESS_WIDTH-1:0] pc
+    input  logic                     int, //trigger 
+    output logic [ADDRESS_WIDTH-1:0] pc_out
 );
 
 logic [ADDRESS_WIDTH-1:0] next_pc;
-
+logic [ADDRESS_WIDTH-1:0] trig_mux_out;
+logic [ADDRESS_WIDTH-1:0] pc;
 pc_mux my_mux (
     .PCsrc(PCsrc),
     .ImmExt(ImmExt),
@@ -19,11 +20,30 @@ pc_mux my_mux (
     .next_pc(next_pc)
 );
 
+
+
 pc_reg my_reg(
     .clk(clk),
     .rst(rst),
     .next_pc(next_pc),
     .pc(pc)
+);
+
+mux trig_mux(
+
+    .input0({ADDRESS_WIDTH{1'b0}}),
+    .input1(pc),
+    .src(int),
+
+    .out(trig_mux_out)
+);
+
+mux rst_mux(
+    .input0(trig_mux_out),
+    .input1({ADDRESS_WIDTH{1'b1}}),
+    .src(rst),
+
+    .out(pc_out)
 );
 
 endmodule
