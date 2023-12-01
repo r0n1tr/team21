6a96075a11d8cecb7eb393c59a5765da2b1f315e
za722-ic <za722@ic.ac.uk>
2023-12-01 02:20:47 +0000

    Update signals (and formatting) of cpu.sv

--- cpu.sv
+++ cpu.sv
@@ -1,6 +1,6 @@
 module cpu #(
     parameter DATA_WIDTH = 32,
-              ADDRESS_WIDTH = 8
+              ADDRESS_WIDTH = 32
 )(
     input logic clk,
     input logic rst,
@@ -15,11 +15,8 @@ module cpu #(
   
 // -- output from top_alu --
 // don't list a0 here since that is output of entire cpu, hence not internal
-logic zero;      // zero flag
-logic aluresult;
-
-// -- output from top_pc --
-logic [ADDRESS_WIDTH-1:0] pc; // program counter 
+logic signed [DATA_WIDTH-1:0] aluresult;
+logic                         zero;      // zero flag
 
 // -- output from control unit --
 // these are all control signals
@@ -37,23 +34,28 @@ logic [DATA_WIDTH-1:0] rd_dm; // instruction word from data memory
 // -- output from instr_mem --
 logic [DATA_WIDTH-1:0] instr; // instruction word from instruction memory
 
-// -- output from reg_file
+// -- output from top_pc --
+logic [ADDRESS_WIDTH-1:0] pc; // program counter 
+
+// -- output from reg_file --
 logic signed [DATA_WIDTH-1:0] rd1;   
 logic signed [DATA_WIDTH-1:0] rd2;    
 
 // -- output from sign_extend --
 logic signed [DATA_WIDTH-1:0] immext; // 32-bit sign extended immediate operand 
 
-// -- outputs from muxes --
-logic signed [DATA_WIDTH-1:0] aluop2;    // output from alu_mux
-logic signed [DATA_WIDTH-1:0] aluresult; // output from alu
+// --output from result_mux -- (the mux that has select == resultsrc)
+logic signed [DATA_WIDTH-1:0] result;
 
-top_alu topalu(
-    .aluop1(rd1),
-    .aluop2(aluop2),
-    .aluresult(aluresult),
+
+top_alu top_alu(
+    .alusrc(alusrc),
     .alucontrol(alucontrol),
+    .rd1(rd1),
+    .rd2(rd2),
+    .immext(immext),
     
+    .aluresult(aluresult),
     .zero(zero)
 );
 
@@ -64,50 +66,52 @@ top_control_unit control_unit(
     .pcsrc(pcsrc),
     .resultsrc(resultsrc),
     .memwrite(memwrite),
-    .alucontrol(alucontrol),
     .alusrc(alusrc),
     .immsrc(immsrc),
-    .regwrite(regwrite)
+    .regwrite(regwrite),
+    .alucontrol(alucontrol)
 );
 
-data_mem DataMemory(
-    .a(aluresult),
-    .wd(rd2),
-    .we(memwrite),
+data_mem data_mem(
     .clk(clk),
+    .we(memwrite),
+    .wd(rd2),
+    .a(aluresult),
 
     .rd(rd_dm)
 );
 
-mux data_mem_mux(
+mux result_mux(
     .input0(aluresult),
     .input1(rd_dm),
-    .src(resultsrc),
+    .select(resultsrc),
+
     .out(result)
 );
 
-instr_mem instrMem(
+instr_mem instr_mem(
     .a(pc),
 
     .rd(instr)
 );
 
-top_pc t_PC(
+top_pc top_PC(
     .clk(clk),
     .rst(rst),
     .pcsrc(pcsrc),
     .immext(immext),
     .trigger(trigger),
-    .pc_out(pc)
+
+    .pc(pc)
 );
 
-reg_file myregfile(
+reg_file reg_file(
+    .clk(clk),
+    .we3(regwrite),
+    .wd3(result),
     .ad1(instr[19:15]),
     .ad2(instr[24:20]),
     .ad3(instr[11:7]),
-    .we3(regwrite),
-    .wd3(result), //
-    .clk(clk),
 
     .rd1(rd1),
     .rd2(rd2),
@@ -120,8 +124,6 @@ sign_extend signExtend(
 
     .immop(immext)
 );
-
-
 
 endmodule
 
