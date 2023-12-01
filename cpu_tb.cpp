99d3156e55a9b0e96977c627e4ce6780cf38ea09
r0n1tr <rr422@ic.ac.uk>
2023-12-01 16:11:42 +0000

    functioning cpu using f1_test.mem file

--- cpu_tb.cpp
+++ cpu_tb.cpp
@@ -20,15 +20,16 @@ int main(int argc, char **argv, char **env) {
     // initialise cpu
     top->clk = 0;
     top->rst = 1;
-    top->trigger = 1;
+    top->trigger = 0;
     
     // run simulation for enough cycles for the CPU to reach 255
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < 100; i++)
     {
         // stop resetting clock after cycle 0
         // cpu will take a few cycles to enter the loop, aftwe which there will be an increment every 3 cycles
         if(i >= 1) {
             top->rst = 0;
+            top->trigger = 1;
         }
         
         // dump variables into VCD file and toggle clock
