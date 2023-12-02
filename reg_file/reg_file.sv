c270f1dae3ef63388d07491bfa1b633e7018a4c3
r0n1tr <rr422@ic.ac.uk>
2023-12-02 11:23:30 +0000

    changed writte signal posedge->negedge

--- reg_file/reg_file.sv
+++ reg_file/reg_file.sv
@@ -16,7 +16,7 @@ module reg_file #(
 
 logic [DATA_WIDTH-1:0] ram_array [2**REG_FILE_ADDR_WIDTH-1:0];
 
-always_ff @(posedge clk) begin
+always_ff @(negedge clk) begin
     if(we3) ram_array[ad3] <= wd3;
 end
 
