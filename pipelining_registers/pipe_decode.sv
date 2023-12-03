23fda2996897a6ea9536a2e9843f5e47ea99e28a
daniald1e <danial.dehghan22@imperial.ac.uk>
2023-12-03 11:21:35 +0000

    created the decode block register for pipelining

---
+++ pipelining_registers/pipe_decode.sv
@@ -0,0 +1,42 @@
+module pipe_execute #(
+    parameters DATA_WIDTH = 32,
+               ADDRESS_WIDTH = 32,
+               WRITE_WIDTH = 5
+
+) (
+    input logic [DATA_WIDTH-1:0] rd1d,    // could be simplified to match what they are called in ALU 
+    input logic [DATA_WIDTH-1:0] rd2d,
+    input logic [ADDRESS_WIDTH-1:0] pcd,
+    input logic [WRITE_WIDTH-1:0] rdd,
+    input logic [DATA_WIDTH-1:0] immexd,
+    input logic [ADDRESS_WIDTH-1:0] pcplus4d,
+
+    
+    output logic [DATA_WIDTH-1:0] rd1e,
+    output logic [DATA_WIDTH-1:0] rd2e,
+    output logic [ADDRESS_WIDTH-1:0] pce,
+    output logic [WRITE_WIDTH-1:0] rde,
+    output logic [DATA_WIDTH-1:0] immexe,
+    output logic [ADDRESS_WIDTH-1:0] pcplus4e,
+
+    input logic clk
+
+);
+
+always_ff @ (posedge clk)
+    begin
+     rd1e <= rd1d;
+     rd2e <= rd2d;
+     pce <= pcf;
+     rde <= rdd;
+     immexe <= immexd;
+     pcplus4e <= pcplus4d; 
+
+    end
+    
+
+
+
+
+
+endmodule
