2cb298b048131f02eb5e189b5880e3d55b676869
daniald1e <danial.dehghan22@imperial.ac.uk>
2023-12-03 11:20:46 +0000

    Created the Fetch Register for pipelining

--- pipelining_registers/pipe_fetch.sv
+++ pipelining_registers/pipe_fetch.sv
@@ -1,20 +1,37 @@
 module pipe_fetch #(
-    parameter ADDRESS_WIDTH = 32,
-    DATA_WIDTH = 32
+    parameter 
+            ADDRESS_WIDTH = 32,
+            DATA_WIDTH = 32,
+            WRITE_WIDTH = 5
+            
+
+              
 )(
-    input logic clk,
-    input logic [DATA_WIDTH-1:0] a,
-    input logic [DATA_WIDTH-1:0] pcplus4f
-    output logic [DATA_WIDTH-1:0] pcf,
-    output logic [DATA_WIDTH-1:0] instrd
-);
+  
+     
+    input logic [DATA_WIDTH-1:0]        rd,   //the data that comes from instruction memory  
+    input logic [ADDRESS_WIDTH-1:0]     pcf,
+    input logic [ADDRESS_WIDTH-1:0]     pcplus4f,
 
-always_ff @ (posedge clk) begin
-     <= next_pc;
-end
+    output logic [DATA_WIDTH-1:0]       instrd,    
+    output logic [ADDRESS_WIDTH-1:0]    pcd,
+    output logic [ADDRESS_WIDTH-1:0]    pcplus4d,
+    output logic [WRITE_WIDTH-1:0]      rdd,
 
-    
+    input logic                         clk
+
+); 
+
+always_ff @ (posedge clk)
+    begin
+        instrd =< rd;
+        pcd <= pcf;
+        pcplus4d <= pcplus4f;
 
 
+    end
+
+assign rdd = instrd[11:7];  //destination register adress 
+ 
 
 endmodule
