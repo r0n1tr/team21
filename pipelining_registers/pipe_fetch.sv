8f9c3d9d55ec297dff2d1a245137b3889f8a1185
r0n1tr <rr422@ic.ac.uk>
2023-12-04 19:11:44 +0000

    added clr and enable signals for hazard unit

--- pipelining_registers/pipe_fetch.sv
+++ pipelining_registers/pipe_fetch.sv
@@ -7,6 +7,8 @@ module pipe_fetch #(
     input logic [DATA_WIDTH-1:0]        rd,   //the data that comes from instruction memory  
     input logic [ADDRESS_WIDTH-1:0]     pcf,
     input logic [ADDRESS_WIDTH-1:0]     pcplus4f,
+    input logic en_n,
+    input logic clr,
 
     output logic [DATA_WIDTH-1:0]       instrd,    
     output logic [ADDRESS_WIDTH-1:0]    pcd,
@@ -18,9 +20,16 @@ module pipe_fetch #(
 
     always_ff @ (posedge clk)
         begin
+        if(clr) begin
+            instrd <= 32'b0;
+            pcd <= 32'b0;
+            pcplus4d <= 32'b0;
+        end
+        else if(en_n == 1'b0) begin
             instrd <= rd;
             pcd <= pcf;
             pcplus4d <= pcplus4f;
         end
+    end
 
 endmodule
