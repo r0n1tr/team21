93c0933bad2bc2557ed29e64b7bc77e6813c56dd
r0n1tr <rr422@ic.ac.uk>
2023-12-08 19:07:36 +0000

    -

--- pc/pc_mux.sv
+++ pc/pc_mux.sv
@@ -11,7 +11,6 @@ module pc_mux #(
     input logic        [ADDRESS_WIDTH-1:0] pc,         // current value of pc (in pc_reg)
     input logic signed [DATA_WIDTH-1:0]    immexte,    // immediate offset (e.g. for branch instruction)
     input logic signed [ADDRESS_WIDTH-1:0] pce,
-//    input logic signed [DATA_WIDTH-1:0]    rd1e,
 
     output logic [ADDRESS_WIDTH-1:0] pcplus4,
     output logic [ADDRESS_WIDTH-1:0] next_pc // new value of pc (to be written to pc_reg)
@@ -21,13 +20,12 @@ module pc_mux #(
 always_comb begin
     pcplus4 = pc + {29'b0, 3'b100}; // add 4
 
-    //logic pctarget = jalr ? rd1e : pce;
     casez ({pcsrc, trigger , rst})
         4'b???1: next_pc = {32{1'b0}};    // set pc to 0
         4'b??00: next_pc = {32{1'b0}};    // set pc to 0
         4'b0010: next_pc = pcplus4;       // next instruction in memory
         4'b0110: next_pc = pce + immexte; // branch, jal
-        4'b1010: next_pc = aluresult;        // jalr
+        4'b1010: next_pc = aluresult;     // jalr
         default: next_pc = {32{1'b1}};
     endcase
 end
