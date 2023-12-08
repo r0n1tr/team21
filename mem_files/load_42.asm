839fc93127af3a0df48a5ace000d7212a3849f28
za722-ic <za722@ic.ac.uk>
2023-12-08 02:22:47 +0000

    Formatting load_42.asm

--- mem_files/load_42.asm
+++ mem_files/load_42.asm
@@ -6,12 +6,12 @@ _boot:
     jal  x1, _loadfortytwo   # goto loadfortytwo
     addi a0, x0, 66          # a0 := 66
     jal  ra, _loadfortytwo   # goto loadfortytwo
-    jal  ra, _halt
+    jal  ra, _halt           # do nothing forever
 
 _loadfortytwo:
     addi a0, x0, 42   # a0 := 42
     jalr x0, ra, 0    # RET
 
-_halt:
+_halt:   
     addi x0, x0, 0    # nop
     jal  x0, _halt
