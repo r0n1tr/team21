0a0ee987095e209db700b0a6ce64a3a3ab645a48
za722-ic <za722@ic.ac.uk>
2023-12-08 00:02:07 +0000

    Update some assembly/machine code

--- mem_files/ringcounter.asm
+++ mem_files/ringcounter.asm
@@ -3,9 +3,9 @@
 
 _boot:    
     addi a0, x0, 1    # a0 := 1
-    jal  x0, _f1      # goto f1
+    jal  x0, _rc      # goto rc
     
-_f1:
+_rc:
     beq a0, x0, _boot
     add a0, a0, a0    # add a0 to itself to left shift
-    jal x0, _f1       # goto f1
+    jal x0, _rc       # goto rc
