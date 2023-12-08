1493eb84ef4dbf7cae75b3b3c469aad6f3ca328a
za722-ic <za722@ic.ac.uk>
2023-12-08 02:17:42 +0000

    Update load_42 program to halt upon completion

--- mem_files/load_42.asm
+++ mem_files/load_42.asm
@@ -2,10 +2,16 @@
 .text
 
 _boot:    
-    addi a0, x0, 21         # a0 := 21
-    jal  ra, loadfortytwo   # goto loadfortytwo
-    addi a0, x0, 66         # a0 := 66
+    addi a0, x0, 21          # a0 := 21
+    jal  x1, _loadfortytwo   # goto loadfortytwo
+    addi a0, x0, 66          # a0 := 66
+    jal  ra, _loadfortytwo   # goto loadfortytwo
+    jal  ra, _halt
 
-loadfortytwo:
+_loadfortytwo:
     addi a0, x0, 42   # a0 := 42
-    ret               # pseudoinstruction for JALR
+    jalr x0, ra, 0    # RET
+
+_halt:
+    addi x0, x0, 0    # nop
+    jal  x0, _halt
