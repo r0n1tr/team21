c3361ec0f6fc94df46df21f67b23e19b08f8b20f
daniald1e <danial.dehghan22@imperial.ac.uk>
2023-12-09 03:57:15 +0000

    implemented two way associative cache added internal u flag corrected data, set and tag bits added if statement for adding data to cache

--- cache/cache_2w.sv
+++ cache/cache_2w.sv
@@ -8,13 +8,13 @@ module cache(
 
 
     output logic [DATA_WIDTH-1:0] dout,
-    output logic hit
+    output logic hit 
 );
 
-logic [26:0] din_tag = din[26:5];
-logic [2:0] din_set = din[4:2];
-
-logic [59:0] cache_memory [SET_WIDTH-1:0];
+logic [27:0] din_tag = din[32:4];   //tag is 28 bits 
+logic [1:0] din_set = din[3:2];     // set is now bits 
+logic u = 1'b0; // initialize to 0 at beginning to determine which item was least recently used 
+logic [119:0] cache_memory [SET_WIDTH-1:0];
 
 
 initial begin
@@ -22,29 +22,44 @@ initial begin
     $readmemh("cache_2w.mem", cache_memory);
 end;
 
-logic [123:0] cache_set = cache_memory[din_set];
+logic [121:0] cache_set = cache_memory[din_set];
 
-logic V_way_1 = cache_set[123:122];
-logic [26:0] cache_way_1_tag = cache_set[121:94];
-logic [31:0] cache_way_1_data = cache_set[93:62];
+logic V_way_1 = cache_set[121];
+logic [27:0] cache_way_1_tag = cache_set[120:93];   
+logic [31:0] cache_way_1_data = cache_set[92:61];
 
-logic V_way_0 = cache_set[61:60];
-logic [26:0] cache_way_0_tag = cache_set[59:32];
+logic V_way_0 = cache_set[60];
+logic [27:0] cache_way_0_tag = cache_set[59:32];
 logic [31:0] cache_way_0_data = cache_set[31:0];
 
 logic hit_1;
 logic hit_0;
 
-assign hit_1 = V_way_1[1] & (din_tag == cache_way_1_tag);
-assign hit_0 = V_way_0[1] & (din_tag == cache_way_0_tag);
+assign hit_1 = V_way_1 & (din_tag == cache_way_1_tag);
+assign hit_0 = V_way_0 & (din_tag == cache_way_0_tag);
 assign hit = hit_0 | hit_1;
 
+always_comb begin
+if (hit_1)begin
+    
+    dout <= cache_way_1_data;
+    u <= 1'b0;
+end                          // least recently used in set is way 0 
 
-if (hit_1)          assign dout = cache_way_1_data;
-else if (hit_0)     assign dout = cache_way_0_data;
-else
+else if (hit_0) begin
+    dout <= cache_way_0_data;
+    u <= 1'b1;
+end 
+else begin 
     assign dout = din;
-    cache_set = {1'b1,din[31:5], rd};
-    cache_memory[din_set] = cache_set;
-
+    if (u) begin 
+        cache_set [121:61] = {1'b1,din[31:4], rd}; 
+        cache_memory[din_set] = cache_set;
+    end 
+    else begin
+    cache_set [60:0] = {1'b1,din[31:4], rd};
+    cacche_memory[din_set] = cache_set;
+    end 
+end 
+end 
 endmodule
