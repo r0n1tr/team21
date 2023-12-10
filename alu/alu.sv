0bac17019a95e9bdeb8ee2edfe0108bd461be3a3
za722-ic <za722@ic.ac.uk>
2023-12-10 09:49:55 +0000

    Formatting alu.sv

--- alu/alu.sv
+++ alu/alu.sv
@@ -14,14 +14,15 @@ always_comb begin
         4'b0000: aluresult = aluop1  +  aluop2;       // add             
         4'b0001: aluresult = aluop1  -  aluop2;       // sub             
         4'b0010: aluresult = aluop1 <<  aluop2[4:0];  // shift left logic
-        4'b0011: aluresult = aluop1  ^  aluop2;       // xor              
-        4'b0100: aluresult = aluop1 >>  aluop2[4:0];  // shift right logical             
-        4'b0101: aluresult = aluop1 >>> aluop2[4:0];  // shift right arithmetic             
-        4'b0110: aluresult = aluop1  |  aluop2;       // or             
-        4'b0111: aluresult = aluop1  &  aluop2;       // and   
-        4'b1000: aluresult = ($signed(aluop1)   < $signed(aluop2)  ) ? 32'b1 : 32'b0;   // set less than  
-        4'b1001: aluresult = ($unsigned(aluop1) < $unsigned(aluop2)) ? 32'b1 : 32'b0;   // set less than unsigned            
-
+        4'b0101: aluresult = aluop1  ^  aluop2;       // xor              
+        4'b0110: aluresult = aluop1 >>  aluop2[4:0];  // shift right logical             
+        4'b0111: aluresult = aluop1 >>> aluop2[4:0];  // shift right arithmetic             
+        4'b1000: aluresult = aluop1  |  aluop2;       // or             
+        4'b1001: aluresult = aluop1  &  aluop2;       // and  
+        
+        4'b0011: aluresult = (  $signed(aluop1) <   $signed(aluop2)) ? 32'b1 : 32'b0;   // set less than  
+        4'b0100: aluresult = ($unsigned(aluop1) < $unsigned(aluop2)) ? 32'b1 : 32'b0;   // set less than unsigned     
+    
         default: aluresult = {32{1'b1}}; // unrecognised alucontrol
     endcase 
     
