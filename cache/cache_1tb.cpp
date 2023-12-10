36ff4f75a22907efdb0018a44a1f64ff48a56293
MohammedTK22 <133985295+MohammedTK22@users.noreply.github.com>
2023-12-10 21:48:00 +0000

    Updated cache_1tb.cpp

--- cache/cache_1tb.cpp
+++ cache/cache_1tb.cpp
@@ -2,8 +2,11 @@
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 #include <iostream>
+#include <vector>
 
 int main(int argc, char **argv, char **env){
+    int i;
+    int clk;
 
     Verilated::commandArgs(argc, argv);
     // init top verilog instance
@@ -12,26 +15,26 @@ int main(int argc, char **argv, char **env){
     Verilated::traceEverOn(true);
     VerilatedVcdC* tfp = new VerilatedVcdC;
     top->trace (tfp, 99);
-    tfp->open ("memory.vcd");
+    tfp->open ("data_mem.vcd");
 
     // initialize simulation inputs
-
+    top->clk = 0;
+    top->we = 0;
+    top->rst = 0;
     // run simulation for many clock cycles
-    for (int i = 0; i<512; i++){
-    
+    for (i=0; i<300; i++){
+
         // dump variables into VCD file and toggle clock
-        for(int clk=0; clk<2; clk++){
+        for(clk=0; clk<2; clk++){
             tfp->dump  (2*i+clk);
             top->clk = !(top->clk);
             top->eval ();
-        }
+            }
 
-        // print output
-        std::cout << "cycle " << i << "  " << (int) top->dout << std::endl;
+        std::cout << "cycle " << i << "  " << "hit " << (int) top->hit << " output " << (int) top->read_data << std::endl;
 
-
-
-        if (Verilated::gotFinish()) exit(0);
+        
+        if (Verilated::gotFinish())     exit(0);
         
     }
       
