65f33e48158f5201b2dc807c0aa1d77229b8d331
r0n1tr <rr422@ic.ac.uk>
2023-12-14 17:40:30 +0000

    little endian changes

--- instr_mem/instr_mem.sv
+++ instr_mem/instr_mem.sv
@@ -15,11 +15,11 @@ logic [BYTE_WIDTH-1:0] rom_array [MEM_SIZE-1:0];
 // program rom with contents of .mem file
 initial begin
     $display("Loading rom...");
-    $readmemh("mem_files/pdf.mem", rom_array); // NOTE: include a blank line after final line of machine code (otherwise the last byte is not read- at least that's what the testbench for this module implies)
+    $readmemh("instr_mem/pdf_littleendian.mem", rom_array); // NOTE: include a blank line after final line of machine code (otherwise the last byte is not read- at least that's what the testbench for this module implies)
 end;
 
 // asynchronous output
 // in this implementation, we assume that instruction memory address is always a multiple of 4, and that it is an error otherwise
-assign instr = {rom_array[a] , rom_array[a+1] , rom_array[a+2] , rom_array[a+3] }; // TODO: Reverse order (+3,+2,+1,+0) to make little endian (i think) (as specified in brief)
+assign instr = {rom_array[a+3] , rom_array[a+2] , rom_array[a+1] , rom_array[a]}; // TODO: Reverse order (+3,+2,+1,+0) to make little endian (i think) (as specified in brief)
 
 endmodule
